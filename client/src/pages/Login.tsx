76bab2de88443045e877f9a820d6a31b0b66944c
yunhwan <sky10240107@naver.com>
2023-05-03 21:04:59 +0900

    feat: Login 레이아웃 구현

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import LoginContainer from '../components/Login/LoginContainer';
 
 function Login() {
-  return <div>Login</div>
+  return <LoginContainer />;
 }
 
-export default Login
+export default Login;
