8e7d7f64df60b6395ed431f3614b5b80d2dd7dbd
yunhwan <sky10240107@naver.com>
2023-05-15 02:30:18 +0900

    feat: quill내 사진 src를 url로 받는 기능 구현

--- client/src/components/Portfolio/DetailBox.tsx
+++ client/src/components/Portfolio/DetailBox.tsx
@@ -1,40 +1,112 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import * as S from './DetailBox.style';
-import ReactQuill from 'react-quill';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import python from 'highlight.js/lib/languages/python';
+import typescript from 'highlight.js/lib/languages/typescript';
+import c from 'highlight.js/lib/languages/c';
+import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { ImageResize } from 'quill-image-resize-module-ts';
+import axios from 'axios';
+
+Quill.register('modules/ImageResize', ImageResize);
 
 type DetailBoxProps = {
   text: string;
+  content: string;
+  setContent: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const modules = {
-  toolbar: {
-    container: [
-      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-      ['blockquote', 'code-block'],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-      [{ direction: 'rtl' }], // text direction
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-      [{ font: [] }],
-      [{ align: [] }],
-      ['clean'],
-      ['blackquote', 'link', 'image'],
-    ],
-  },
-};
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('c', c);
 
-const DetailBox: React.FC<DetailBoxProps> = ({ text }) => {
-  const setting =
-    '<p><br></p><h3><strong>*본 형식은 작성을 돕는 가이드일 뿐입니다, 자유로운 형식으로 작성해보세요*</strong></h3><p><br></p><h3>기간 : 2023.00.00 - 2023.00.00</h3><p><br></p><h3>Overview ｜ 프로젝트 요약</h3><p>본인이 진행한 프로젝트에서 어떤 일을 진행했는지 알 수 있도록 간결하게 설명해주세요</p><p><br></p><h3>Role ｜ 역할</h3><p>해당 프로젝트에서 본인이 기여한 정도와 역할을 작성해 주세요.</p><p>기여도 : 00%</p><p><br></p><h3>Function ｜ 기능</h3><p> 작동 화면과 간단한 설명으로 프로젝트에서 구현한 기능에 대해서 설명해주세요.</p><p><br></p><p><br></p><h3>Result ｜ 성과</h3><p>해당 프로젝트의 성과를 작성해 주세요. 수치, 숫자로 성과를 표현할 수 있다면 설득력이 높아져요. 수치로 표현하기 어려운 성과라면 정성적인 성과도 괜찮아요.</p><p><br></p><p><br></p><h3>Lesson-Learned ｜ 프로젝트 후 배운 것</h3><p>모든 프로젝트가 성공하지 못했더라도, 배운 점은 있을 거예요. 해당 프로젝트에서 배운 점, 아쉬웠던 점, 개선할 수 있는 점을 적어서 다음 프로젝트 진행 시에 이를 보완할 수 있다는 것을 어필해 보세요.</p>';
+const DetailBox: React.FC<DetailBoxProps> = ({ text, content, setContent }) => {
+  const quillRef = useRef<ReactQuill | null>(null);
+  // 이미지 처리를 하는 핸들러
+  const imageHandler = () => {
+    console.log('에디터에서 이미지 버튼을 클릭하면 이 핸들러가 시작됩니다!');
+    if (!quillRef.current) return;
+    const quillInstance: any = quillRef.current.getEditor();
+    // 1. 이미지를 저장할 input type=file DOM을 만든다.
+    const input = document.createElement('input');
+    // 속성 써주기
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click(); // 에디터 이미지버튼을 클릭하면 이 input이 클릭된다.
+    // input이 클릭되면 파일 선택창이 나타난다.
 
-  const [content, setContent] = useState(setting);
-  console.log(content);
+    input.onchange = async () => {
+      const file = input.files![0];
+      const formData = new FormData();
+      formData.append('image', file);
+
+      // 이미지를 서버로 전송 (서버 엔드포인트 주소를 사용)
+      const response = await axios.post('YOUR_SERVER_API_URL', {
+        body: formData,
+      });
+
+      const imageUrl = await response;
+      const range = quillInstance.getSelection(true);
+      quillInstance.insertEmbed(range.index, 'image', imageUrl);
+    };
+  };
+
+  const modules = useMemo(() => {
+    return {
+      // syntax: {
+      //   highlight: (text: any) => hljs.highlightAuto(text).value,
+      // },
+      toolbar: {
+        container: [
+          [{ header: [1, 2, false] }, { header: '2' }, { font: [String] }],
+          [{ size: [String] }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+          ['link', 'image', 'code-block'],
+          ['clean'],
+        ],
+        // handlers: {
+        //   image: imageHandler,
+        // },
+      },
+      // 이미지 조절 시 오류
+      ImageResize: {
+        modules: ['Resize', 'DisplaySize', 'Toolbar'],
+      },
+    };
+  }, []);
+
+  const formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+    'video',
+    'code-block',
+  ];
+
   return (
     <S.DetailContainer>
       <S.Title>{text}</S.Title>
-      <ReactQuill modules={modules} onChange={setContent} value={content} />
+      <ReactQuill
+        ref={quillRef}
+        modules={modules}
+        onChange={setContent}
+        value={content}
+        theme='snow'
+      />
     </S.DetailContainer>
   );
 };
