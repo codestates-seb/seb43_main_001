31c1cd80cc7dcb9f8b3a5129a985a08494632e2c
khkh0109 <ist200120@gmail.com>
2023-05-19 13:23:26 +0900

    feat: GeneraLayout에 oulet 추가

--- client/src/layout/GeneraLayout.tsx
+++ client/src/layout/GeneraLayout.tsx
@@ -1,15 +1,17 @@
+import { Outlet } from 'react-router-dom';
+
 import Header from './header/Header';
 import Footer from './footer/Footer';
 import Main from './main/Main';
-interface GeneralLayoutProps {
-  children: React.ReactNode;
-}
+
 // 필요에 따라서 outlet을 사용할 수도 있음
-const GeneraLayout: React.FC<GeneralLayoutProps> = ({ children }) => {
+const GeneraLayout = () => {
   return (
     <>
       <Header />
-      <Main>{children}</Main>
+      <Main>
+        <Outlet />
+      </Main>
       <Footer />
     </>
   );
