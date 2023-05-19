48a13f211f5ad1c14dd5b1224f1a4e8af7fe991e
uknow <98323914+yunhoknight@users.noreply.github.com>
2023-05-19 18:00:07 +0900

    Feat: User&Security 기능 수정 및 추가 User&Security 기능 수정 및 추가 - user : isAuth 기능 수정 - security : RefreshToken 연관관계 수정 Issue #138

--- server/src/main/java/main001/server/security/jwt/RefreshToken.java
+++ server/src/main/java/main001/server/security/jwt/RefreshToken.java
@@ -2,12 +2,11 @@ package main001.server.security.jwt;
 
 import lombok.*;
 import main001.server.audit.BaseTimeEntity;
+import main001.server.domain.user.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.persistence.CascadeType;
 import java.util.Collection;
 
 @Entity
@@ -21,7 +20,14 @@ public class RefreshToken extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private long userId;
     private String userIp;
     private String refreshToken;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    public RefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
