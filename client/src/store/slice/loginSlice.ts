de7727b33e799468c27b4b8b3e44939a22fa5ed8
rickyshu <hoinleekk@naver.com>
2023-05-20 13:09:14 +0900

    refactor:loginSlice 재수정

--- client/src/store/slice/loginSlice.ts
+++ client/src/store/slice/loginSlice.ts
@@ -1,23 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '..';
 
 type Login = {
   isLogin: boolean;
+  accessToken: string | null;
+  refreshToken: string | null;
 };
 
-const initialState: Login = { isLogin: false };
+const initialState: Login = { isLogin: false, accessToken: null, refreshToken: null };
 const loginSlice = createSlice({
   name: 'login',
   initialState,
   reducers: {
-    login: (state) => {
+    login: (state, action: PayloadAction<{ accessToken: string; refreshToken: string }>) => {
       state.isLogin = true;
+      state.accessToken = action.payload.accessToken;
+      state.refreshToken = action.payload.refreshToken;
     },
-    logout: (state) => {
+    logout: (state, action: PayloadAction<null>) => {
       state.isLogin = false;
+      state.accessToken = null;
+      state.refreshToken = null;
+    },
+    setAccessToken: (state, action: PayloadAction<{ accessToken: string }>) => {
+      state.accessToken = action.payload.accessToken;
     },
   },
 });
 
-export const { login, logout } = loginSlice.actions;
+export const refresh = (state: RootState) => state.login.refreshToken;
+export const access = (state: RootState) => state.login.accessToken;
+export const loginState = (state: RootState) => state.login.isLogin;
+
+export const { login, logout, setAccessToken } = loginSlice.actions;
 
 export default loginSlice.reducer;
