1db55ba2ef9a4c20a237cf256701a7c33b41e465
xyz-wr <pponi13468@naver.com>
2023-05-22 16:06:19 +0900

    representativeImg null 값 기본 이미지로 대체

--- server/src/main/java/main001/server/domain/portfolio/controller/PortfolioController.java
+++ server/src/main/java/main001/server/domain/portfolio/controller/PortfolioController.java
@@ -76,6 +76,7 @@ public class PortfolioController {
     }
 
 
+
     @DeleteMapping("/img-delete/{imgId}")
     public ResponseEntity deleteImg(@PathVariable Long imgId) {
         portfolioService.deleteImage(imgId);
@@ -87,7 +88,6 @@ public class PortfolioController {
         List<String> imageUrlList = portfolioService.getImageUrlList();
         return ResponseEntity.ok(imageUrlList);
     }
-
 
     @PatchMapping("/{portfolio-id}")
     public ResponseEntity patchPortfolio(@PathVariable("portfolio-id") long portfolioId,
