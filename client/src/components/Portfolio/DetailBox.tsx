d338348e43aca9600799cc8629bfb5f917a57e3f
yunhwan <sky10240107@naver.com>
2023-05-22 17:43:14 +0900

    feat: cotent 내 이미지 전송 기능 구현

--- client/src/components/Portfolio/DetailBox.tsx
+++ client/src/components/Portfolio/DetailBox.tsx
@@ -6,9 +6,10 @@ import 'highlight.js/styles/rainbow.css';
 
 import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import axios from 'axios';
+
 import { ImageActions } from '@xeger/quill-image-actions';
 import { ImageFormats } from '@xeger/quill-image-formats';
+import { PortfolioAPI } from '../../api/client';
 
 Quill.register('modules/imageActions', ImageActions);
 Quill.register('modules/imageFormats', ImageFormats);
@@ -27,30 +28,58 @@ const DetailBox: React.FC<DetailBoxProps> = ({ text, content, setContent }) => {
   const quillRef = useRef<ReactQuill | null>(null);
   // 이미지 처리를 하는 핸들러
   const imageHandler = () => {
-    console.log('에디터에서 이미지 버튼을 클릭하면 이 핸들러가 시작됩니다!');
     if (!quillRef.current) return;
+    console.log(typeof quillRef.current.getEditor());
     const quillInstance: any = quillRef.current.getEditor();
-    // 1. 이미지를 저장할 input type=file DOM을 만든다.
+    // 이미지를 저장할 input type=file DOM을 만든다.
     const input = document.createElement('input');
     // 속성 써주기
     input.setAttribute('type', 'file');
     input.setAttribute('accept', 'image/*');
     input.click(); // 에디터 이미지버튼을 클릭하면 이 input이 클릭된다.
-    // input이 클릭되면 파일 선택창이 나타난다.
 
+    // input이 클릭되면 파일 선택창이 나타난다.
     input.onchange = async () => {
       const file = input.files![0];
-      const formData = new FormData();
-      formData.append('image', file);
+
+      try {
+        // 이미지 업로드
+        const url = await PortfolioAPI.uploadImg(file);
+        console.log(url);
+        const range = quillInstance.getSelection(true);
+        quillInstance.insertEmbed(range.index, 'image', url);
+        quillInstance.setSelection(range.index + 1);
+      } catch (error) {
+        console.log(error);
+      }
+
+      // ! 1 FileReader 방식
+      // const reader = new FileReader();
+      // reader.onloadend = function () {
+      //   // console.log(reader.result);
+
+      //   const range = quillInstance.getSelection(true);
+
+      //   quillInstance.insertEmbed(range.index, 'image', reader.result);
+      //   quillInstance.setSelection(range.index + 1);
+      // };
+      // reader.readAsDataURL(file);
+
+      // ! 2 createObjectURL 방식
+      // const blob = new Blob([file]);
+      // const url = URL.createObjectURL(blob);
+      // console.log(url);
+      // const range = quillInstance.getSelection(true);
+      // quillInstance.insertEmbed(range.index, 'image', url);
+      // console.log(range);
+      // quillInstance.setSelection(range.index + 1);
+
+      // setContentImgs((prev) => [...prev, file]);
 
       // 이미지를 서버로 전송 (서버 엔드포인트 주소를 사용)
-      const response = await axios.post('YOUR_SERVER_API_URL', {
-        body: formData,
-      });
-
-      const imageUrl = await response;
-      const range = quillInstance.getSelection(true);
-      quillInstance.insertEmbed(range.index, 'image', imageUrl);
+      // const response = await axios.post('http://43.201.157.191:8080/portfolios/52/img-upload', {
+      //   body: formData,
+      // });
     };
   };
 
@@ -104,10 +133,10 @@ const DetailBox: React.FC<DetailBoxProps> = ({ text, content, setContent }) => {
           ['clean'],
         ],
 
-        // // 내부 이미지를 url로 받아오는 handler
-        // handlers: {
-        //   image: imageHandler,
-        // },
+        // 내부 이미지를 url로 받아오는 handler
+        handlers: {
+          image: imageHandler,
+        },
       },
     };
   }, []);
