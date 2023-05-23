0a61ca66dc085c52a12862f3bdbe4c83996b1f34
938938 <938kjh@gmail.com>
2023-05-23 21:35:19 +0900

    style: console.log() 제거

--- client/src/pages/User.tsx
+++ client/src/pages/User.tsx
@@ -4,7 +4,6 @@ import UserInfo from '../components/User/UserInfo';
 import { useParams } from 'react-router-dom';
 
 function User() {
-  // TODO : 해당 유저 데이터의 id값으로 정보, 포트폴리오, 코멘트 모두 가져오기
   const { userId } = useParams() as { userId: string };
 
   return (
