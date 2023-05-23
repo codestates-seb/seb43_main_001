5177653ad8471a704e23c005d1554c2f2c913b47
938938 <938kjh@gmail.com>
2023-05-23 21:36:50 +0900

    design: CSS 수정

--- client/src/components/User/UserEditForm.style.ts
+++ client/src/components/User/UserEditForm.style.ts
@@ -48,10 +48,14 @@ export const EditSelect = styled.select`
   border: 1px solid black;
   font-size: 0.9rem;
   border-radius: 5px;
+  color: ${({ theme }) => theme.themeStyle.fontColor};
+  background-color: ${({ theme }) => theme.themeStyle.backgroundColor};
+  cursor: pointer;
+
   option {
-    color: red;
     width: 100%;
-    background-color: white;
+    color: ${({ theme }) => theme.themeStyle.fontColor};
+    background-color: ${({ theme }) => theme.themeStyle.backgroundColor};
   }
 `;
 export const EmailIcon = styled(RxEnvelopeClosed)``;
