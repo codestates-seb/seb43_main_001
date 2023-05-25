1fa5fc1fded4d8428d37790c75cc3dc1a17d8628
khkh0109 <ist200120@gmail.com>
2023-05-25 11:30:12 +0900

    design: 배너 일러스트 추가 및 파비콘 변경

--- client/src/components/Home/Banner.style.ts
+++ client/src/components/Home/Banner.style.ts
@@ -6,42 +6,53 @@ export const Banner = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 70px;
+  padding: 50px 15px;
   width: 100%;
-  height: 508px;
-  padding: 0 15px;
+  height: 100%;
+  background-color: #f7f7f7;
+
+  @media ${({ theme }) => theme.breakpoints.DESKTOPMIN} {
+    flex-direction: row;
+    gap: 120px;
+    height: 500px;
+    padding: 0 15px;
+  }
+`;
+
+export const BannerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   color: #000;
-  background-color: #f0f0f0;
   text-align: center;
 
   h1 {
-    margin-bottom: 20px;
-    font-size: 50px;
+    margin-bottom: 5px;
+    font-size: 40px;
     font-weight: 700;
   }
 
   h2 {
-    margin-bottom: 20px;
-    font-size: 20px;
+    margin-bottom: 30px;
+    font-size: 17px;
     font-weight: 500;
   }
 
-  p {
-    margin-bottom: 55px;
-    font-size: 14px;
-    font-weight: 400;
-    word-break: keep-all;
-  }
-
   @media ${({ theme }) => theme.breakpoints.TABLETMIN} {
-    height: 650px;
+    h1 {
+      font-size: 48px;
+    }
 
-    p {
-      font-size: 24px;
+    h2 {
+      font-size: 18px;
     }
   }
 
   @media ${({ theme }) => theme.breakpoints.DESKTOPMIN} {
-    height: 700px;
+    text-align: left;
+    align-items: flex-start;
 
     h1 {
       font-size: 68px;
@@ -52,4 +63,18 @@ export const Banner = styled.section`
 export const ShareButton = styled(YellowBtn)`
   width: 180px;
   height: 60px;
+`;
+
+export const BannerImg = styled.div`
+  width: 300px;
+  height: auto;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  @media ${({ theme }) => theme.breakpoints.TABLETMIN} {
+    width: 400px;
+  }
 `;
--- client/src/components/Home/Banner.tsx
+++ client/src/components/Home/Banner.tsx
@@ -16,18 +16,17 @@ function Banner() {
 
   return (
     <S.Banner>
-      <h1>Silmul</h1>
-      <h2>
-        실력을 물어보자! <br />
-        개발자들의 포트폴리오 공유 플랫폼, 실물
-      </h2>
-      {/* <p>
-        실물(Silmul)은 개발자들을 위한 소통하는 포트폴리오 공유 플랫폼입니다.
-        <br /> 실물(Silmul)에서는 프로젝트, 기술 및 경험을 포함하여 포트폴리오에 대한 세부 정보를
-        추가할 수 있습니다. <br />
-        포트폴리오를 검색하고 다른 개발자의 작업을 확인해보세요!
-      </p> */}
-      <S.ShareButton onClick={handleClickButton}>프로젝트 공유하기</S.ShareButton>
+      <S.BannerContent>
+        <h1>Silmul</h1>
+        <h2>
+          실력을 물어보자! <br />
+          개발자들의 포트폴리오 공유 플랫폼, 실물
+        </h2>
+        <S.ShareButton onClick={handleClickButton}>프로젝트 공유하기</S.ShareButton>
+      </S.BannerContent>
+      <S.BannerImg>
+        <img src='./assets/image/meeting.svg' alt='배경 일러스트' />
+      </S.BannerImg>
     </S.Banner>
   );
 }
